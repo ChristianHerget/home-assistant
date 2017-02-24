3f6cddfca75fd1062ffac65bf8a13ab2f92be8ba
Christian <ch.herget@gmail.com>
2017-02-25 00:19:51 +0100

    And even more code clean up

--- homeassistant/components/climate/avm_homeautomation.py
+++ homeassistant/components/climate/avm_homeautomation.py
@@ -10,7 +10,8 @@ import logging
 import voluptuous as vol
 import homeassistant.helpers.config_validation as cv
 
-from homeassistant.components.climate import (STATE_AUTO, STATE_OFF, STATE_ON, ClimateDevice)
+from homeassistant.components.climate import (
+    STATE_AUTO, STATE_OFF, STATE_ON, ClimateDevice)
 from homeassistant.const import (ATTR_TEMPERATURE, TEMP_CELSIUS, STATE_UNKNOWN)
 from homeassistant.components.avm_homeautomation import (
     ATTR_DISCOVER_DEVICES, DATA_AVM_HOMEAUTOMATION, DOMAIN,
@@ -21,20 +22,24 @@ _LOGGER = logging.getLogger(__name__)
 
 DEPENDENCIES = ['avm_homeautomation']
 
+
 @asyncio.coroutine
-def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+def async_setup_platform(hass, config, async_add_entities,
+                         discovery_info=None):
     """Setup the avm_smarthome switch platform."""
-                
+
     if discovery_info is None:
         return
     else:
-        __ains  = discovery_info[ATTR_DISCOVER_DEVICES]
+        __ains = discovery_info[ATTR_DISCOVER_DEVICES]
 
-        for __ain  in __ains:
+        for __ain in __ains:
             __aha = hass.data[DATA_AVM_HOMEAUTOMATION][DOMAIN]
             _LOGGER.debug("Adding device '%s'" % __ain)
-            yield from async_add_entities([AvmThermostat(hass, __ain, __aha)], update_before_add=True)
-                
+            yield from async_add_entities(
+                [AvmThermostat(hass, __ain, __aha)],
+                update_before_add=True)
+
     return
 
 SCHEMA_DICT_CLIMATE = vol.Schema({
@@ -60,51 +65,52 @@ THERMOSTAT_STATES = [STATE_AUTO, STATE_MANUAL, STATE_ON, STATE_OFF]
 
 ATTR_BATTERY_STATE = "Battery"
 
-STATE_BATTERY_OK  = "Ok"
+STATE_BATTERY_OK = "Ok"
 STATE_BATTERY_LOW = "Low"
 
 BATTERY_STATES = [STATE_BATTERY_OK, STATE_BATTERY_LOW]
 
 HM_ATTRIBUTE_SUPPORT = {
-    'lock':       ['lock',       {0: 'No', 1: 'Yes'}],
+    'lock':       ['lock', {0: 'No', 1: 'Yes'}],
     'devicelock': ['devicelock', {0: 'No', 1: 'Yes'}],
-    'errorcode':  ['errorcode',  {}],
-    'batterylow': ['Battery',    {0: 'Normal', 1: 'Low'}],
+    'errorcode':  ['errorcode', {}],
+    'batterylow': ['Battery', {0: 'Normal', 1: 'Low'}],
 }
+
 
 class AvmThermostat(AvmHomeAutomationDevice, ClimateDevice):
     """Representation of a AVM Thermostat."""
-    
+
     def _validate_schema(self, value):
         SCHEMA_DICT_CLIMATE(value)
-        
+
     @property
     def unit_of_measurement(self):
         """The unit of measurement to display."""
         return self.hass.config.units.temperature_unit
-                
+
     @property
     def state(self) -> str:
         """Return the state of the entity."""
         return self.current_operation
-    
+
     @property
     def temperature_unit(self):
         """Return the unit of measurement."""
         return TEMP_CELSIUS
-        
+
     @property
     def current_operation(self):
         """Return the current operation. head, cool idle."""
         __operation = STATE_UNKNOWN
-        if self.available == False:
+        if not self.available:
             __operation = STATE_UNKNOWN
-        elif (self.target_temperature == self.target_temperature_high) or\
-            (self.target_temperature == self.target_temperature_low):
+        elif ((self.target_temperature == self.target_temperature_high) or
+                (self.target_temperature == self.target_temperature_low)):
             __operation = STATE_AUTO
         else:
             __target = int(self._dict['hkr']['tsoll'])
-        
+
             if __target == 253:
                 __operation = STATE_OFF
             elif __target == 254:
@@ -118,23 +124,23 @@ class AvmThermostat(AvmHomeAutomationDevice, ClimateDevice):
     def operation_list(self):
         """Return the operation modes list."""
         return THERMOSTAT_STATES
-    
+
     @property
     def target_temperature(self) -> float:
         __target = int(self._dict['hkr']['tsoll'])
-        
+
         if __target == 253:
             __target = 0
         elif __target == 254:
             __target = 60
-            
+
         return self._convert_for_display(float(__target) / 2.0)
-    
+
     @property
     def target_temperature_high(self) -> float:
         return self._convert_for_display(
             float(int(self._dict['hkr']['komfort'])) / 2.0)
-        
+
     @property
     def target_temperature_low(self) -> float:
         return self._convert_for_display(
@@ -144,16 +150,17 @@ class AvmThermostat(AvmHomeAutomationDevice, ClimateDevice):
     def temperature(self):
         """Return the _temperature we try to reach."""
         return self.taget_temperature
-    
+
     @asyncio.coroutine
     def async_set_temperature(self, **kwargs):
         """Set new target _temperature."""
         temperature = kwargs.get(ATTR_TEMPERATURE)
         temperature = int(temperature * 2.0)
-        yield from self._aha.async_send_switch_command({'switchcmd': 'sethkrtsoll', 'param': str(temperature)}, self._ain)
+        yield from self._aha.async_send_switch_command(
+            {'switchcmd': 'sethkrtsoll', 'param': str(temperature)}, self._ain)
         self._dict['hkr']['tsoll'] = temperature
         return
-    
+
     @asyncio.coroutine
     def async_set_operation_mode(self, operation_mode):
         """Set operation mode (auto, cool, heat, off)."""
@@ -165,12 +172,13 @@ class AvmThermostat(AvmHomeAutomationDevice, ClimateDevice):
             temperature = self._dict['hkr']['komfort']
         else:
             return
-        
-        yield from self._aha.async_send_switch_command({'switchcmd': 'sethkrtsoll', 'param': str(temperature)}, self._ain)
+
+        yield from self._aha.async_send_switch_command(
+            {'switchcmd': 'sethkrtsoll', 'param': str(temperature)}, self._ain)
         self._dict['hkr']['tsoll'] = temperature
-            
+
         return
-    
+
     @property
     def min_temp(self):
         """Return the minimum temperature."""
@@ -180,7 +188,7 @@ class AvmThermostat(AvmHomeAutomationDevice, ClimateDevice):
     def max_temp(self):
         """Return the maximum temperature."""
         return self._convert_for_display(28.0)
-    
+
     @property
     def device_state_attributes(self):
         """Return the optional state attributes."""
@@ -189,13 +197,13 @@ class AvmThermostat(AvmHomeAutomationDevice, ClimateDevice):
         # no data available to create
         if not self.available:
             return attrs
-                
+
         # Generate an attributes list
         for node, data in HM_ATTRIBUTE_SUPPORT.items():
             # Is an attributes and exists for this object
             if node in self._dict['hkr']:
-                value = data[1].get(int(self._dict['hkr'][node]), str(self._dict['hkr'][node]))
+                value = data[1].get(
+                    int(self._dict['hkr'][node]), str(self._dict['hkr'][node]))
                 attrs[data[0]] = value
- 
-        return attrs
 
+        return attrs
--- homeassistant/components/switch/avm_homeautomation.py
+++ homeassistant/components/switch/avm_homeautomation.py
@@ -12,30 +12,35 @@ import homeassistant.helpers.config_validation as cv
 
 from homeassistant.const import (STATE_ON, STATE_OFF, STATE_UNKNOWN)
 from homeassistant.components.switch import (SwitchDevice)
-from homeassistant.components.avm_homeautomation import (AvmHomeAutomationDevice, 
-    ATTR_DISCOVER_DEVICES, DATA_AVM_HOMEAUTOMATION, DOMAIN,
-    SCHEMA_DICT_SWITCH, SCHEMA_DICT_POWERMETER, SCHEMA_DICT_TEMPERATURE,
-    SCHEMA_DICT_HKR)
+from homeassistant.components.avm_homeautomation import (
+    AvmHomeAutomationDevice, ATTR_DISCOVER_DEVICES, DATA_AVM_HOMEAUTOMATION,
+    DOMAIN, SCHEMA_DICT_SWITCH, SCHEMA_DICT_POWERMETER,
+    SCHEMA_DICT_TEMPERATURE, SCHEMA_DICT_HKR)
 
 _LOGGER = logging.getLogger(__name__)
 
 DEPENDENCIES = ['avm_homeautomation']
 
+
 @asyncio.coroutine
-def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+def async_setup_platform(hass, config, async_add_entities,
+                         discovery_info=None):
     """Setup the avm_smarthome switch platform."""
 
     if discovery_info is None:
         return
     else:
-        __ains  = discovery_info[ATTR_DISCOVER_DEVICES]
+        __ains = discovery_info[ATTR_DISCOVER_DEVICES]
 
         # print(hass.data[DATA_AVM_HOMEAUTOMATION][DOMAIN])
-        
-        for __ain  in __ains:
+
+        for __ain in __ains:
             __aha = hass.data[DATA_AVM_HOMEAUTOMATION][DOMAIN]
             _LOGGER.debug("Adding device '%s'" % __ain)
-            yield from async_add_entities([AvmHomeAutomationDeviceSwitch(hass, __ain, __aha)], update_before_add=True)
+            yield from async_add_entities(
+                [AvmHomeAutomationDeviceSwitch(hass, __ain, __aha)],
+                update_before_add=True
+                )
     return
 
 ATTR_CURRENT_CONSUMPTION = 'Current Consumption'
@@ -65,54 +70,57 @@ VAL_SCHEMA_DICT_SWITCH = vol.Schema({
         }, extra=vol.ALLOW_EXTRA)
 
 HM_ATTRIBUTE_SUPPORT = {
-    'mode':       ['mode',       {}],
-    'lock':       ['lock',       {0: 'No', 1: 'Yes'}],
+    'mode':       ['mode', {}],
+    'lock':       ['lock', {0: 'No', 1: 'Yes'}],
     'devicelock': ['devicelock', {0: 'No', 1: 'Yes'}],
 }
 
+
 class AvmHomeAutomationDeviceSwitch(AvmHomeAutomationDevice, SwitchDevice):
     """Representation of a FRITZ!DECT switch."""
-    
+
     def _validate_schema(self, value):
         VAL_SCHEMA_DICT_SWITCH(value)
-    
+
     @property
     def current_power_watt(self):
         """Return the current power usage in Watt."""
-        return float(int(self._dict['powermeter']['power'])) / 1000.0 # mW to W
-    
+        # mW to W
+        return float(int(self._dict['powermeter']['power'])) / 1000.0
+
     @property
     def total_energy_watt_hours(self):
         """Return the energy usage in Watt Hours."""
         return int(self._dict['powermeter']['energy'])
-    
+
     @property
     def total_energy_killo_watt_hours(self):
         """Return the energy usage in Killo Watt Hours."""
-        return float(int(self._dict['powermeter']['energy'])) / 1000.0 # Wh to kWh
-    
-    ## Import from Entity
+        # Wh to kWh
+        return float(int(self._dict['powermeter']['energy'])) / 1000.0
+
+    # # Import from Entity
     @property
     def state(self) -> str:
-        """Return the state of the entity."""       
-        if self.available == True:
-            if self.is_on == True:
+        """Return the state of the entity."""
+        if self.available:
+            if self.is_on:
                 return STATE_ON
             else:
                 return STATE_OFF
         else:
             return STATE_UNKNOWN
-        
+
     @property
     def assumed_state(self) -> bool:
         """Return True if unable to access real state of the entity."""
         return False
-    
+
     @property
     def device_state_attributes(self):
         """Return the state attributes of the device."""
         attrs = {}
-        
+
         # no data available to create
         if not self.available:
             return attrs
@@ -121,38 +129,44 @@ class AvmHomeAutomationDeviceSwitch(AvmHomeAutomationDevice, SwitchDevice):
             (self.current_power_watt, ATTR_CURRENT_CONSUMPTION_UNIT)
         attrs[ATTR_TOTAL_CONSUMPTION] = "%.3f %s" % \
             (self.total_energy_killo_watt_hours, ATTR_TOTAL_CONSUMPTION_UNIT)
-        attrs[ATTR_TEMPERATURE] = "%.1f %s" % (self.current_temperature, self.units.temperature_unit)     
-                
+        attrs[ATTR_TEMPERATURE] = "%.1f %s" % \
+            (self.current_temperature, self.units.temperature_unit)
+
         # Generate an attributes list
         for node, data in HM_ATTRIBUTE_SUPPORT.items():
             # Is an attributes and exists for this object
             if node in self._dict['switch']:
                 value = str(self._dict['switch'][node])
                 try:
-                    value = data[1].get(int(self._dict['switch'][node]), str(self._dict['switch'][node]))
+                    value = data[1].get(
+                        int(self._dict['switch'][node]),
+                        str(self._dict['switch'][node])
+                        )
                 except Exception:
                     pass
                 attrs[data[0]] = value
- 
+
         return attrs
-    
-    ## Import from ToggleEntity
+
+    # # Import from ToggleEntity
     @property
     def is_on(self) -> bool:
         """Return True if entity is on."""
-        return bool(int(self._dict['switch']['state']))     
-        
+        return bool(int(self._dict['switch']['state']))
+
     @asyncio.coroutine
     def async_turn_on(self, **kwargs) -> None:
         """Turn the entity on."""
-        yield from self._aha.async_send_switch_command({'switchcmd': 'setswitchon'}, self._ain)
+        yield from self._aha.async_send_switch_command(
+            {'switchcmd': 'setswitchon'}, self._ain)
         self._dict['switch']['state'] = True
 
     @asyncio.coroutine
     def async_turn_off(self, **kwargs) -> None:
         """Turn the entity off."""
-        yield from self._aha.async_send_switch_command({'switchcmd': 'setswitchoff'}, self._ain)
+        yield from self._aha.async_send_switch_command(
+            {'switchcmd': 'setswitchoff'}, self._ain)
         self._dict['switch']['state'] = False
-            
-    ## Import from SwitchDevice
-    ## None
+
+    # # Import from SwitchDevice
+    # # None
